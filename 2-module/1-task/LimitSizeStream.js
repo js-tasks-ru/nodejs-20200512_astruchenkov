8da95c5190aaedd5c68b8bbba6f0161da07ed6a3
Alexey Struchenkov <forcorp@inbox.ru>
2020-05-19 22:35:04 +0200

    Реализован LimitSizeStream

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,10 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.bufferSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    // LimitExceededError
+    this.bufferSize += chunk.length;
+    if (this.bufferSize > this.limit) {
+      this.destroy(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
