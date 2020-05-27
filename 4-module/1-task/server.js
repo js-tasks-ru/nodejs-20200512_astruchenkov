d8acb7d2887f06c92042cd96d2f9e24571d8bc45
Alexey Struchenkov <forcorp@inbox.ru>
2020-05-27 22:40:25 +0200

    Решение задачи 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,16 +1,33 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
+  if (pathname.split('/').length > 1) {
+    res.statusCode = 400;
+    res.end('Не поддерживаются вложенные пути');
+  }
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      fs.createReadStream(filepath)
+          .on('error', (error) => {
+            switch (error.code) {
+              case 'ENOENT':
+                res.statusCode = 404;
+                res.end('Файл не найден');
+                break;
+              default:
+                res.statusCode = 500;
+                res.end('ошибка сервера');
+            }
+          })
+          .pipe(res);
 
       break;
 
