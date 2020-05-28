00578a56ebf8ec0df5e81210d15be8644b7bbf9c
Alexey Struchenkov <forcorp@inbox.ru>
2020-05-28 15:30:10 +0200

    Решение задачи 5-module 1-task

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -8,10 +8,34 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscribers = {};
+const getMessage = new Promise((resolve, reject) => {
+});
+
 router.get('/subscribe', async (ctx, next) => {
+  const id = Math.random();
+  subscribers[id] = ctx;
+  try {
+    await getMessage;
+  } catch (e) {
+
+  }
 });
 
 router.post('/publish', async (ctx, next) => {
+  const msg = ctx.request.body.message;
+  if (msg) {
+    for (const [id, client] of Object.entries(subscribers)) {
+      client.status = 200;
+      client.res.on('close', (err)=>{
+        delete subscribers[id];
+      });
+      client.res.end(msg);
+    }
+
+    ctx.status = 200;
+    ctx.body = msg;
+  }
 });
 
 app.use(router.routes());
