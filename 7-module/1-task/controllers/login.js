d6ab9ba42aae4db881e73533fac8d38c48fb205a
Alexey Struchenkov <forcorp@inbox.ru>
2020-06-08 22:16:37 +0200

    Добавлено решение задачаи 7-module 1-task

--- 7-module/1-task/controllers/login.js
+++ 7-module/1-task/controllers/login.js
@@ -4,7 +4,6 @@ const passport = require('../libs/passport');
 module.exports.login = async function login(ctx, next) {
   await passport.authenticate('local', async (err, user, info) => {
     if (err) throw err;
-
     if (!user) {
       ctx.status = 400;
       ctx.body = {error: info};
