e3d036c23122d7453a90af71810d7659812f5477
Alexey Struchenkov <forcorp@inbox.ru>
2020-06-09 14:24:24 +0200

    Добавлено решение 7-module 2-task

--- 7-module/2-task/libs/strategies/local.js
+++ 7-module/2-task/libs/strategies/local.js
@@ -1,8 +1,30 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-  {usernameField: 'email', session: false},
-  function(email, password, done) {
-    done(null, false, 'Стратегия подключена, но еще не настроена');
-  }
+    {usernameField: 'email', session: false},
+    async function(email, password, done) {
+      if (email) {
+        let user = await User.findOne({email});
+        if (user) {
+          if (!await user.checkPassword(password)) {
+            return done(null, false, 'Неверный пароль');
+          }
+        } else {
+          try {
+            user = await new User({
+              email,
+              displayName: email.split('@')[0],
+            });
+            await user.setPassword(password);
+
+            await user.save();
+          } catch (e) {
+            if (e.name !== 'ValidationError') return done(e);
+            return done(null, false, e.errors.email.message);
+          }
+        }
+        return done(null, user, null);
+      }
+    }
 );
