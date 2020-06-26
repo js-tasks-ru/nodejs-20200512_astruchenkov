f892ea211f747b5d683cd192e27829fc98fb0fe7
Alexey Struchenkov <forcorp@inbox.ru>
2020-06-26 14:15:16 +0200

    Исправлено решение задачи 9-module 1-task

--- 9-module/1-task/controllers/messages.js
+++ 9-module/1-task/controllers/messages.js
@@ -4,11 +4,9 @@ const mapMessage = require('../mappers/message');
 
 module.exports.messageList = async function messages(ctx, next) {
   try {
-    const messages = await Message.find({chat: ctx.user._id}) || [];
-    if (messages) {
-      ctx.status = 200;
-      ctx.body = {messages: messages.map(mapMessage)};
-    }
+    const messages = await Message.find({chat: ctx.user._id}).limit(20);
+    ctx.status = 200;
+    ctx.body = {messages: messages.map(mapMessage)};
   } catch (e) {
     console.log(e);
     throw e;
